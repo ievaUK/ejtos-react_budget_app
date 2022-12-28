887858d6c5f0cb06a49f561373956dc506c4e713
IevaUK <ievgr@inbox.lv>
2022-12-28 13:41:42 -0500

    add UX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Add code to import the components
 import { AppProvider } from './context/AppContext';
+import AllocationForm from './components/AllocationForm';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import Budget from './components/Budget';
+import ExpenseList from './components/ExpenseList';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 {/* Add the code here to add the components          */}
+                <h1 class="py-3">Company's budget allocation</h1>
+                <div class='row'>
+                    <Budget/>
+                    <Remaining />
+                    <ExpenseTotal />
+                </div>
+                <div>
+                    <h2>Allocation</h2>
+                    <ExpenseList/>
+                </div>
+            
+                <h2 class="py-2">Change Allocation</h2>
+                <AllocationForm />
             </div>
         </AppProvider>
     );
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,21 @@
 
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Remaining = () => {
+    const { expenses, budget } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
+    return (
+        <div className={`alert ${alertType} col-md-3 mx-3`}>
+            <span>Remaining: £{budget - totalExpenses}</span>
+        </div>
+    );
+};
+
+export default Remaining;
